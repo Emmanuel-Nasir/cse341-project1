e84033e46fdb80695b08fe9c99ea1644005e497c
Emmanuel Nasir <emmanuel.nasi@gmail.com>
2025-11-22 13:43:16 -0800

    installed swagger

--- controllers/users.js
+++ controllers/users.js
@@ -11,6 +11,7 @@ const result = await mongodb.getDatabase().collection('users').find()
     });
 };
 const getsingle = async (req, res) => {
+    
     const userId = new objectId(req.params.id);
     // const result = await mongodb.getDatabase().db().collection('users').find({_id: userId});
     
@@ -22,7 +23,55 @@ const getsingle = async (req, res) => {
     });
 };
 
+const createUser = async (req, res) => {
+    // #swagger.tags = ['users'];
+    // Implementation for creating a user
+    const user={
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().collection('users').insertOne(user)
+    if (response.insertedId ) {
+        res.status(201).json({_id: response.insertedId});
+    } else {
+    res.status(500).json(response.error || 'Some error occurred while creating the user.');
+    }
+    };
+
+const updateUser = async (req, res) => {
+    // #swagger.tags = ['users'];
+    const userId = new objectId(req.params.id);
+    const user={
+         firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().collection('users').updateOne({_id: userId}, {$set: user});
+    if (response.modifiedCount > 0) {
+        res.status( 204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while updating the user.');
+    }
+};
+const deleteUser = async (req, res) => {
+    // #swagger.tags = ['users'];
+    const userId = new objectId(req.params.id);
+    const response = await mongodb.getDatabase().collection('users').deleteOne({_id: userId} );
+    if (response.deletedCount > 0) {
+        res.status( 204).send(); 
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while deleting the user.');
+    }
+};
 module.exports = {
     getAll,
-    getsingle
+    getsingle,
+    createUser,
+    updateUser,
+    deleteUser
 };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+router.use('/', require('./swagger'));
 
-//router.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
+router.get('/', (req, res) => {
+    // #swagger.tags = ['Hello world'];
+  res.send('Hello World!');
+});
 router.use('/users',require('./users'));
 module.exports = router;
